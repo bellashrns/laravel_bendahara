36d4785f94c49a5f94af727ddb158a567c521a1e
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-04-10 15:54:22 +0700

    coba ubah contoh.blade

--- resources/views/contoh.blade.php
+++ resources/views/contoh.blade.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>contoh</title>
+    <title>Contoh</title>
 </head>
 <body>
     <h1 class="text-3xl font-bold underline">
-      Hello world!
+      Contoh push
     </h1>
 </body>
 </html>
