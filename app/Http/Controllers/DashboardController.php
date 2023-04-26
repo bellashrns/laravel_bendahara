7d8f10cef7ca1ee3cd5a0ab8461869edc5c2123f
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-04-26 23:34:01 +0700

    showing users list on /user page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -18,6 +19,10 @@ class DashboardController extends Controller
 
     public function user()
     {
-        return view('dashboard.user');
+        $users = User::all();
+        
+        return view('dashboard.user', [
+            'users' => $users
+        ]);
     }
 }
--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,18 @@
     @vite('resources/css/app.css')
     <title>UMN Radio | Anggota</title>
 </head>
+
 <body>
     <h1>list user</h1>
+    <ul>
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+            <li>{{ $user->email }}</li>
+            <li>{{ $user->phone }}</li>
+            <li>{{ $user->address }}</li>
+            <li>{{ $user->line }}</li>
+            <li>{{ $user->birthdate }}</li>
+        @endforeach
 </body>
+
 </html>
