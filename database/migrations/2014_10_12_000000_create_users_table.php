2512da05146d2bcae7d8fcbf7a051755ba0af9cc
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-04-29 01:02:25 +0700

    fixing nullable property on image db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('address');
             $table->string('line')->unique();
             $table->date('birthdate');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
