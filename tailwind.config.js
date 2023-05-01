a1bef5baf31401f84ebc12aa2dca7ff621a4eb7a
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-05-01 13:58:38 +0700

    build(developer): :package: add daisyUI
    now tailwind can be used with daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php", 	
-    "./resources/**/*.jsx",		
-    "./resources/**/*.vue",	
-    "./node_modules/flowbite/**/*.js"	
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin'), plugin(function({ addBase, theme }) {
-      addBase({
-        'h1': { fontSize: theme('fontSize.2xl'), fontWeight: 700 },
-        'h2': { fontSize: theme('fontSize.xl'), fontWeight: 700 },
-        'h3': { fontSize: theme('fontSize.lg'), fontWeight: 700 },
-      })
-    })
-  ], 
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.jsx",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("flowbite/plugin"),
+        require("@tailwindcss/forms"),
+        require("daisyui"),
+        plugin(function ({ addBase, theme }) {
+            addBase({
+                h1: { fontSize: theme("fontSize.2xl"), fontWeight: 700 },
+                h2: { fontSize: theme("fontSize.xl"), fontWeight: 700 },
+                h3: { fontSize: theme("fontSize.lg"), fontWeight: 700 },
+            });
+        }),
+    ],
+};
