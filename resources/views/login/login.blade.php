52f94492fdb8df25689d4c808a052f5dc97185e8
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-05-02 15:26:42 +0700

    hihi maaf

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -39,7 +39,8 @@
                             class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
                     </div>
                     <div class="relative w-full min-w-[200px] text-right">
-                        <a class="text-blue-600 hover:text-blue-500" href="{{ route('password.request') }}">Forgot Password?</a>
+                        <a class="text-blue-600 hover:text-blue-500" href="{{ route('password.request') }}">Forgot
+                            Password?</a>
                     </div>
                     <div class="relative w-full min-w-[200px] h-11 mb-3">
                         <button type="submit"
